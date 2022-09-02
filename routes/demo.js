dd7fdfa193784a4d76f101d221fd314c40097ac4
Ritik raj <sinharitikraj@gmail.com>
2022-09-02 21:28:34 +0530

    login repo

---
+++ routes/demo.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// const db = require('../data/database');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('signup');
+});
+
+router.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+router.get('/login', function (req, res) {
+  res.render('login');
+});
+
+router.post('/signup', async function (req, res) {});
+
+router.post('/login', async function (req, res) {});
+
+
+
+router.post('/logout', function (req, res) {});
+
+module.exports = router;
